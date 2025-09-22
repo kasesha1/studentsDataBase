dda394664b40343b370cb71f9086cae162c350ab
kasesha1 <153167184+kasesha1@users.noreply.github.com>
2025-09-22 20:42:10 +0300

    Изменение в файле main.cpp
    Добавлены 6 тестов:
    Тест 1: Сортировка по имени (по возрастанию)
    Тест 2: Сортировка по имени (по убыванию)
    Тест 3: Сортировка по среднему баллу (по возрастанию)
    Тест 4: Сортировка по среднему баллу (по убыванию)
    Тест 5: Пустая база данных (граничный случай)
    Тест 6: Одинаковые имена (дополнительный тест)

--- main.cpp
+++ main.cpp
@@ -93,6 +93,84 @@ void showSortMenu() {
     std::cout << "Выберите действие: ";
 }
 
+void runTests() {
+    std::cout << "\n=== ЗАПУСК ТЕСТОВ ===\n";
+    
+    // Тест 1: Сортировка по имени (по возрастанию)
+    std::vector<Student> testDb1 = {
+        {"Иван", 20, "Прогрммист", 73.8},
+        {"Анастасия", 21, "Химик", 84.0},
+        {"Станислав", 19, "Инженер", 73.5}
+    };
+    
+    sortByName(testDb1);
+    assert(testDb1[0].name == "Анастасия");
+    assert(testDb1[1].name == "Иван");
+    assert(testDb1[2].name == "Станислав");
+    std::cout << "✔ Тест 1: Сортировка по имени (А-Я) пройден\n";
+    
+    // Тест 2: Сортировка по имени (по убыванию)
+    std::vector<Student> testDb2 = {
+        {"Иван", 20, "Прогрммист", 73.8},
+        {"Анастасия", 21, "Химик", 84.0},
+        {"Станислав", 19, "Инженер", 73.5}
+    };
+    
+    sortByNameDesc(testDb2);
+    assert(testDb2[0].name == "Станислав");
+    assert(testDb2[1].name == "Иван");
+    assert(testDb2[2].name == "Анастасия");
+    std::cout << "✔ Тест 2: Сортировка по имени (Я-А) пройден\n";
+    
+    // Тест 3: Сортировка по среднему баллу (по возрастанию)
+    std::vector<Student> testDb3 = {
+        {"Иван", 20, "Прогрммист", 73.8},
+        {"Анастасия", 21, "Химик", 84.0},
+        {"Станислав", 19, "Инженер", 73.5}
+    };
+    
+    sortByGPA(testDb3);
+    assert(testDb3[0].gpa == 73.5);
+    assert(testDb3[1].gpa == 73.8);
+    assert(testDb3[2].gpa == 84.0);
+    std::cout << "✔ Тест 3: Сортировка по GPA (возрастание) пройден\n";
+    
+    // Тест 4: Сортировка по среднему баллу (по убыванию)
+    std::vector<Student> testDb4 = {
+        {"Иван", 20, "Прогрммист", 73.8},
+        {"Анастасия", 21, "Химик", 84.0},
+        {"Станислав", 19, "Инженер", 73.5}
+    };
+    
+    sortByGPADesc(testDb4);
+    assert(testDb4[0].gpa == 84.0);
+    assert(testDb4[1].gpa == 73.8);
+    assert(testDb4[2].gpa == 73.5);
+    std::cout << "✔ Тест 4: Сортировка по GPA (убывание) пройден\n";
+    
+    // Тест 5: Пустая база данных (граничный случай)
+    std::vector<Student> emptyDb;
+    sortByName(emptyDb);    // Не должно вызывать ошибок
+    sortByGPADesc(emptyDb); // Не должно вызывать ошибок
+    std::cout << "✔ Тест 5: Работа с пустой базой данных пройден\n";
+    
+    // Тест 6: Одинаковые имена (дополнительный тест)
+    std::vector<Student> sameNamesDb = {
+        {"Иван", 20, "Прогрммист", 73.8},
+        {"Иван", 21, "Химик", 84.0},
+        {"Анастасия", 19, "Инженер", 73.5}
+    };
+    
+    sortByName(sameNamesDb);
+    assert(sameNamesDb[0].name == "Анастасия");
+    assert(sameNamesDb[1].name == "Иван");
+    assert(sameNamesDb[2].name == "Иван");
+    std::cout << "✔ Тест 6: Сортировка с одинаковыми именами пройден\n";
+    
+    std::cout << "=== ВСЕ 6 ТЕСТОВ ПРОЙДЕНЫ УСПЕШНО! ===\n\n";
+}
+
+
 int main() {
     std::vector<Student> database;
 
@@ -102,6 +180,7 @@ int main() {
         std::cout << "1. Добавить студента\n";
         std::cout << "2. Вывести список студентов\n";
         std::cout << "3. Сортировать студентов\n";
+        std::cout << "4. Запустить тесты\n";
         std::cout << "0. Выход\n";
         std::cout << "Выберите действие: ";
         std::cin >> choice;
@@ -141,6 +220,9 @@ int main() {
                 } while (sortChoice != 0);
                 break;
             }
+            case 4:
+                runTests();
+                break; 
             case 0:
                 std::cout << "Выход из программы.\n";
                 break;
